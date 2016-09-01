f6d52b0c329dec344b2de1bff8e15ae22fcc4c0b
GaaraDeadPuppet <ikani_93@hotmail.com>
2016-09-02 00:22:47 +0200

    js

--- js/principal.js
+++ js/principal.js
@@ -3,5 +3,19 @@ function darclick(){
 }
 
 function descripcion(){
-	alert("aaaaaaaa");
+	$.ajax({
+		type: "POST",
+		url: "http://www.ilazkitaldea.com/app/php/descripcion_actividades.php",
+		crossDomain: true,
+		cache: false,
+		success: function(result){
+			var result = $.parseJSON(result);
+			$.each(result, function(i,field){
+				$("#results").append(field);
+			});
+		}
+	})
+	.fail(function(jqXHR, textStatus) {
+		$("#results").html( "Request failed: " + textStatus );
+	});
 }
